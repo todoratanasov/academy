a00d7872124a59091678cfa80a3a312fd55f730a
todoratanasov <t.atanasov.atanasov@gmail.com>
2019-06-27 11:53:19 +0300

    upload with little css

--- first_grade_problem/server/public/helpers/toggle-layout.js
+++ first_grade_problem/server/public/helpers/toggle-layout.js
@@ -1,16 +1,18 @@
-import{getData} from "./storage";
-exports.toggle = ()=>{
-    const username = getData("username");
-      //we show/hide buttons depending on if the user is loggedin
-      if(username){
-        document.getElementById("logout").removeAttribute("hidden");
-        document.getElementById("standings").removeAttribute("hidden");
-        document.getElementById("profile").removeAttribute("hidden");
-        document.getElementById("game").removeAttribute("hidden");
+import { getData } from "./storage";
+exports.toggle = () => {
+  const username = getData("username");
+  //we show/hide buttons depending on if the user is loggedin
+  if (username) {
+    document.getElementById("logout").removeAttribute("class", "hidden");
+    document.getElementById("standings").removeAttribute("class", "hidden");
+    document.getElementById("profile").removeAttribute("class", "hidden");
+    document.getElementById("game").removeAttribute("class", "hidden");
 
-        document.getElementById("register").setAttribute("hidden","true");
-        document.getElementById("login").setAttribute("hidden","true");
+    document.getElementById("register").setAttribute("class", "hidden");
+    document.getElementById("login").setAttribute("class", "hidden");
 
-        document.getElementById("greeting").innerText = `Hello, ${username}! It's time to play the game!`;
-      }
-}
+    document.getElementById(
+      "greeting"
+    ).innerText = `Hello, ${username}! It's time to play the game!`;
+  }
+};
